f7f96803abc2d769f9395cce559a5781755f7509
Miron Brezuleanu <mbrezu@gmail.com>
2015-01-14 21:47:05 +0200

    Added client library code and license notes.

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,3 +1,24 @@
+/*
+Copyright (c) 2015, Miron Brezuleanu
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 package;
 
 
@@ -34,14 +55,34 @@ class ServerState implements IServerState {
 	}
 	
 	public function getState(): String {
-		return "hello \n";
+		return "hello";
 	}
 	public function handleCommand(command: String): String {
-		return command + "\n";
+		return 'echo: $command';
 	}
 	public function mainLoop(): String {
 		Sys.sleep(0.01);
 		return null;
+	}
+}
+
+class ClientState implements IClientState {	
+	
+	var counter: Int;
+	public var client: Client;
+	
+	public function new() {		
+		counter = 0;
+	}
+	
+	public function handleUpdate(update: String) {
+		counter ++;
+		trace(counter, update);
+		if (counter == 3 && client != null) {
+			client.shutdown();
+		} else {
+			client.sendCommand("test");
+		}
 	}
 }
 
@@ -54,10 +95,12 @@ class Main extends Sprite {
 		
 		super ();
 		trace("server");
-		var server = new Server(COMMANDS_PORT, UPDATES_PORT, new ServerState() );
+		var server = new Server(COMMANDS_PORT, UPDATES_PORT, new ServerState());
 		//server.shutDown();
 		var btnClient = new Button("Client", function() {
-			var client = new Client("127.0.0.1", COMMANDS_PORT, UPDATES_PORT);
+			var clientState = new ClientState();
+			var client = new Client("127.0.0.1", COMMANDS_PORT, UPDATES_PORT, clientState);
+			clientState.client = client;
 		});
 		addChild(btnClient);
 		var btnShutdown = new Button("Restart Server", function() {
@@ -68,6 +111,4 @@ class Main extends Sprite {
 		btnShutdown.x = 200;
 		addChild(btnShutdown);
 	}
-	
-	
 }
--- Source/me/mbrezu/hxmp/Client.hx
+++ Source/me/mbrezu/hxmp/Client.hx
@@ -1,3 +1,24 @@
+/*
+Copyright (c) 2015, Miron Brezuleanu
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 package me.mbrezu.hxmp;
 
 import sys.net.Host;
@@ -9,33 +30,53 @@ import neko.vm.Thread;
 import cpp.vm.Thread;
 #end
 
+interface IClientState {
+	function handleUpdate(update: String): Void;
+}
+
 class Client
 {
-	public function new(hostName: String, portCommands: Int, portUpdates: Int)  
+	private var clientState: IClientState;
+	private var updatesSocket: Socket;
+	private var commandsSocket: Socket;
+	private var updatesThread: Thread;
+	
+	public function new(hostName: String, portCommands: Int, portUpdates: Int, clientState: IClientState)  
 	{
-		Thread.create(function() {
-			trace("client");
-			var host = new Host(hostName);
-			
-			var updatesSocket = new Socket();
-			updatesSocket.connect(host, portUpdates);
-			updatesSocket.setFastSend(true);
-			
-			var commandsSocket: Socket = new Socket();
-			commandsSocket.connect(host, portCommands);
-			commandsSocket.setFastSend(true);
-			Utils.writeString(commandsSocket, "test");
-			Utils.writeString(commandsSocket, "test");
-			commandsSocket.close();
-			
-			trace("l1", Utils.readString(updatesSocket));
-			Utils.writeString(updatesSocket, "ack");
-			trace("l2", Utils.readString(updatesSocket));
-			Utils.writeString(updatesSocket, "ack");
-			trace("l3", Utils.readString(updatesSocket));
-			Utils.writeString(updatesSocket, "ack");
-			updatesSocket.close();					
-		});
+		this.clientState = clientState;
+		
+		var host = new Host(hostName);
+		updatesSocket = new Socket();
+		updatesSocket.connect(host, portUpdates);
+		commandsSocket = new Socket();
+		commandsSocket.connect(host, portCommands);
+		
+		updatesThread = Thread.create(updateProc);
+	}
+	
+	public function shutdown() {
+		updatesThread.sendMessage(false);
+	}
+	
+	public function sendCommand(command: String) {
+		Utils.writeString(commandsSocket, command);
+	}
+	
+	private function updateProc() {
+		while(true) {
+			updatesSocket.setTimeout(0.1);
+			try {
+				var update = Utils.readString(updatesSocket);
+				Utils.writeString(updatesSocket, "ack");
+				clientState.handleUpdate(update);
+			} catch (any: Dynamic) {
+				if (Thread.readMessage(false) == false) {
+					updatesSocket.close();
+					commandsSocket.close();
+					return;
+				}
+			}
+		}
 	}
 	
 }
